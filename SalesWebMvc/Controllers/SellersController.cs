45dc7131417bb462ca6b90aa3b4e31d32215c979
Gabriel Matias <gmatias1793@gmail.com>
2020-12-22 19:16:29 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -49,6 +49,23 @@ namespace SalesWebMvc.Controllers
             return View(seller);
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seller = _sellerService.FindById(id.Value);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
